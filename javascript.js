9e256a12c9693a333921ced5f7f262b727a8ab60
Sharad Tembhurne <sharadtembhurne97@gmail.com>
2023-03-26 03:00:44 -0400

    Update

--- javascript.js
+++ javascript.js
@@ -94,3 +94,25 @@ function progressBar() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
+
+//Jobs job-container
+function openTab(evt, tabNumber) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(tabNumber).style.display = "inline";
+    evt.currentTarget.className += " active";
+}
