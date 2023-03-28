687e181f11a997d5d251af41b9a4875c354f1873
Sharad Tembhurne <sharadtembhurne97@gmail.com>
2023-03-28 03:34:29 -0400

    counter added

--- javascript.js
+++ javascript.js
@@ -140,3 +140,16 @@ function sendMail() {
         )
 }
 
+
+//Visitor counter
+var counterContainer = document.querySelector(".visit-counter");
+var visitCount = localStorage.getItem("page_view");
+visitCount = 1;
+
+//Add entry for key="page_view"
+localStorage.setItem("page_view", 1);
+visitCount = Number(visitCount) + 1;
+
+// Update local storage value
+localStorage.setItem("page_view", visitCount);
+counterContainer.innerHTML = visitCount;
